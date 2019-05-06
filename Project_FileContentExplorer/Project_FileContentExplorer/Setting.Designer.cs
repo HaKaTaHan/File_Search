9bf91552450317dc74cb76d88911739eaba0d067
CrewDaniel <alaskay@naver.com>
2019-05-06 15:36:43 +0900

    Setting_SearchScope Fixed

--- Project_FileContentExplorer/Project_FileContentExplorer/Setting.Designer.cs
+++ Project_FileContentExplorer/Project_FileContentExplorer/Setting.Designer.cs
@@ -41,19 +41,23 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.treeView1 = new System.Windows.Forms.TreeView();
             this.label5 = new System.Windows.Forms.Label();
             this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.CheckID_Btn = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(78)))));
+            this.panel2.Controls.Add(this.CheckID_Btn);
+            this.panel2.Controls.Add(this.textBox1);
             this.panel2.Controls.Add(this.label11);
             this.panel2.Controls.Add(this.label9);
             this.panel2.Controls.Add(this.label10);
@@ -66,7 +70,6 @@
             this.panel2.Controls.Add(this.comboBox2);
             this.panel2.Controls.Add(this.comboBox1);
             this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.treeView1);
             this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.checkBox4);
             this.panel2.Controls.Add(this.checkBox3);
@@ -84,7 +87,7 @@
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Lato", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.White;
-            this.label11.Location = new System.Drawing.Point(435, 376);
+            this.label11.Location = new System.Drawing.Point(435, 271);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(16, 16);
             this.label11.TabIndex = 30;
@@ -95,7 +98,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Lato", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(718, 381);
+            this.label9.Location = new System.Drawing.Point(718, 276);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(11, 16);
             this.label9.TabIndex = 29;
@@ -106,7 +109,7 @@
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Lato", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.White;
-            this.label10.Location = new System.Drawing.Point(578, 381);
+            this.label10.Location = new System.Drawing.Point(578, 276);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(11, 16);
             this.label10.TabIndex = 28;
@@ -151,7 +154,7 @@
             "29",
             "30",
             "31"});
-            this.comboBox4.Location = new System.Drawing.Point(595, 373);
+            this.comboBox4.Location = new System.Drawing.Point(595, 268);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(121, 24);
             this.comboBox4.TabIndex = 27;
@@ -194,7 +197,7 @@
             "1992",
             "1991",
             "1990"});
-            this.comboBox5.Location = new System.Drawing.Point(735, 373);
+            this.comboBox5.Location = new System.Drawing.Point(735, 268);
             this.comboBox5.Name = "comboBox5";
             this.comboBox5.Size = new System.Drawing.Size(121, 24);
             this.comboBox5.TabIndex = 26;
@@ -219,7 +222,7 @@
             "October",
             "November",
             "December"});
-            this.comboBox6.Location = new System.Drawing.Point(455, 373);
+            this.comboBox6.Location = new System.Drawing.Point(455, 268);
             this.comboBox6.Name = "comboBox6";
             this.comboBox6.Size = new System.Drawing.Size(121, 24);
             this.comboBox6.TabIndex = 25;
@@ -229,7 +232,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Lato", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(289, 381);
+            this.label8.Location = new System.Drawing.Point(289, 276);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(11, 16);
             this.label8.TabIndex = 24;
@@ -240,7 +243,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Lato", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(149, 381);
+            this.label7.Location = new System.Drawing.Point(149, 276);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(11, 16);
             this.label7.TabIndex = 23;
@@ -285,7 +288,7 @@
             "29",
             "30",
             "31"});
-            this.comboBox3.Location = new System.Drawing.Point(166, 373);
+            this.comboBox3.Location = new System.Drawing.Point(166, 268);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 24);
             this.comboBox3.TabIndex = 22;
@@ -328,7 +331,7 @@
             "1992",
             "1991",
             "1990"});
-            this.comboBox2.Location = new System.Drawing.Point(306, 373);
+            this.comboBox2.Location = new System.Drawing.Point(306, 268);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 24);
             this.comboBox2.TabIndex = 21;
@@ -353,7 +356,7 @@
             "October",
             "November",
             "December"});
-            this.comboBox1.Location = new System.Drawing.Point(26, 373);
+            this.comboBox1.Location = new System.Drawing.Point(26, 268);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 20;
@@ -363,18 +366,11 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Lato", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(22, 336);
+            this.label6.Location = new System.Drawing.Point(22, 231);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(146, 24);
             this.label6.TabIndex = 18;
             this.label6.Text = "SEARCH DATE";
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(26, 156);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(486, 156);
-            this.treeView1.TabIndex = 17;
             // 
             // label5
             // 
@@ -446,6 +442,31 @@
             this.label4.TabIndex = 11;
             this.label4.Text = "EXTENSION";
             // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(78)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(26, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(500, 24);
+            this.textBox1.TabIndex = 31;
+            // 
+            // CheckID_Btn
+            // 
+            this.CheckID_Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(50)))), ((int)(((byte)(170)))));
+            this.CheckID_Btn.FlatAppearance.BorderSize = 0;
+            this.CheckID_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CheckID_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckID_Btn.ForeColor = System.Drawing.Color.White;
+            this.CheckID_Btn.Location = new System.Drawing.Point(546, 163);
+            this.CheckID_Btn.Margin = new System.Windows.Forms.Padding(0);
+            this.CheckID_Btn.Name = "CheckID_Btn";
+            this.CheckID_Btn.Size = new System.Drawing.Size(170, 25);
+            this.CheckID_Btn.TabIndex = 32;
+            this.CheckID_Btn.Text = "CHANGE LOCATION";
+            this.CheckID_Btn.UseVisualStyleBackColor = false;
+            // 
             // Setting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -476,12 +497,14 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.CheckBox checkBox4;
         private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.Button CheckID_Btn;
     }
 }
