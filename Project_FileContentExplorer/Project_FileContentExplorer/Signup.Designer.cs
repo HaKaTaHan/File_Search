15edbb64c600e680051733d9425777b042a64bce
Kim <rnlcksgrp1@naver.com>
2019-05-07 11:06:46 +0900

    Re Back_End

--- Project_FileContentExplorer/Project_FileContentExplorer/Signup.Designer.cs
+++ Project_FileContentExplorer/Project_FileContentExplorer/Signup.Designer.cs
@@ -50,9 +50,9 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(61, 45);
+            this.label6.Location = new System.Drawing.Point(70, 56);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(268, 31);
+            this.label6.Size = new System.Drawing.Size(336, 39);
             this.label6.TabIndex = 21;
             this.label6.Text = "CREATE ACCOUNT";
             // 
@@ -61,9 +61,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(63, 125);
+            this.label1.Location = new System.Drawing.Point(72, 156);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 24);
+            this.label1.Size = new System.Drawing.Size(36, 29);
             this.label1.TabIndex = 22;
             this.label1.Text = "ID";
             // 
@@ -72,9 +72,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(63, 225);
+            this.label2.Location = new System.Drawing.Point(72, 281);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 24);
+            this.label2.Size = new System.Drawing.Size(151, 29);
             this.label2.TabIndex = 23;
             this.label2.Text = "PASSWORD";
             // 
@@ -83,20 +83,24 @@
             this.ID_text.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(78)))));
             this.ID_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ID_text.ForeColor = System.Drawing.Color.White;
-            this.ID_text.Location = new System.Drawing.Point(67, 155);
+            this.ID_text.Location = new System.Drawing.Point(77, 194);
+            this.ID_text.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ID_text.Name = "ID_text";
-            this.ID_text.Size = new System.Drawing.Size(250, 24);
+            this.ID_text.Size = new System.Drawing.Size(285, 29);
             this.ID_text.TabIndex = 25;
+            this.ID_text.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ID_text_KeyPress);
+            this.ID_text.Leave += new System.EventHandler(this.ID_text_Leave);
             // 
             // PW_text
             // 
             this.PW_text.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(78)))));
             this.PW_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PW_text.ForeColor = System.Drawing.Color.White;
-            this.PW_text.Location = new System.Drawing.Point(67, 255);
+            this.PW_text.Location = new System.Drawing.Point(77, 319);
+            this.PW_text.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.PW_text.Name = "PW_text";
             this.PW_text.PasswordChar = '*';
-            this.PW_text.Size = new System.Drawing.Size(250, 24);
+            this.PW_text.Size = new System.Drawing.Size(285, 29);
             this.PW_text.TabIndex = 26;
             this.PW_text.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -105,9 +109,9 @@
             this.PW_Check_Label.AutoSize = true;
             this.PW_Check_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PW_Check_Label.ForeColor = System.Drawing.Color.Red;
-            this.PW_Check_Label.Location = new System.Drawing.Point(64, 383);
+            this.PW_Check_Label.Location = new System.Drawing.Point(73, 479);
             this.PW_Check_Label.Name = "PW_Check_Label";
-            this.PW_Check_Label.Size = new System.Drawing.Size(177, 18);
+            this.PW_Check_Label.Size = new System.Drawing.Size(220, 24);
             this.PW_Check_Label.TabIndex = 28;
             this.PW_Check_Label.Text = "* Passwords don\'t match!";
             this.PW_Check_Label.Visible = false;
@@ -117,9 +121,9 @@
             this.ID_Error.AutoSize = true;
             this.ID_Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ID_Error.ForeColor = System.Drawing.Color.Red;
-            this.ID_Error.Location = new System.Drawing.Point(64, 182);
+            this.ID_Error.Location = new System.Drawing.Point(73, 228);
             this.ID_Error.Name = "ID_Error";
-            this.ID_Error.Size = new System.Drawing.Size(129, 18);
+            this.ID_Error.Size = new System.Drawing.Size(165, 24);
             this.ID_Error.TabIndex = 29;
             this.ID_Error.Text = "* Already exists ID!";
             this.ID_Error.Visible = false;
@@ -131,12 +135,14 @@
             this.CheckID_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CheckID_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CheckID_Btn.ForeColor = System.Drawing.Color.White;
-            this.CheckID_Btn.Location = new System.Drawing.Point(343, 154);
+            this.CheckID_Btn.Location = new System.Drawing.Point(392, 192);
+            this.CheckID_Btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CheckID_Btn.Name = "CheckID_Btn";
-            this.CheckID_Btn.Size = new System.Drawing.Size(85, 25);
+            this.CheckID_Btn.Size = new System.Drawing.Size(97, 31);
             this.CheckID_Btn.TabIndex = 30;
             this.CheckID_Btn.Text = "CHECK";
             this.CheckID_Btn.UseVisualStyleBackColor = false;
+            this.CheckID_Btn.Click += new System.EventHandler(this.CheckID_Btn_Click);
             // 
             // SingUp_Btn
             // 
@@ -145,21 +151,23 @@
             this.SingUp_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SingUp_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SingUp_Btn.ForeColor = System.Drawing.Color.White;
-            this.SingUp_Btn.Location = new System.Drawing.Point(67, 540);
+            this.SingUp_Btn.Location = new System.Drawing.Point(77, 675);
+            this.SingUp_Btn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.SingUp_Btn.Name = "SingUp_Btn";
-            this.SingUp_Btn.Size = new System.Drawing.Size(100, 30);
+            this.SingUp_Btn.Size = new System.Drawing.Size(114, 38);
             this.SingUp_Btn.TabIndex = 31;
             this.SingUp_Btn.Text = "SIGN UP";
             this.SingUp_Btn.UseVisualStyleBackColor = false;
+            this.SingUp_Btn.Click += new System.EventHandler(this.SingUp_Btn_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(118, 129);
+            this.label7.Location = new System.Drawing.Point(135, 161);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(199, 18);
+            this.label7.Size = new System.Drawing.Size(248, 24);
             this.label7.TabIndex = 32;
             this.label7.Text = "6 to 10 Characters, Numbers";
             // 
@@ -168,9 +176,9 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(188, 229);
+            this.label8.Location = new System.Drawing.Point(215, 286);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(129, 18);
+            this.label8.Size = new System.Drawing.Size(163, 24);
             this.label8.TabIndex = 33;
             this.label8.Text = "Any kind of 8 to 12";
             // 
@@ -179,9 +187,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(63, 325);
+            this.label3.Location = new System.Drawing.Point(72, 406);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 24);
+            this.label3.Size = new System.Drawing.Size(125, 29);
             this.label3.TabIndex = 24;
             this.label3.Text = "CONFIRM";
             // 
@@ -190,21 +198,24 @@
             this.Confirm_text.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(78)))));
             this.Confirm_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Confirm_text.ForeColor = System.Drawing.Color.White;
-            this.Confirm_text.Location = new System.Drawing.Point(67, 356);
+            this.Confirm_text.Location = new System.Drawing.Point(77, 445);
+            this.Confirm_text.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Confirm_text.Name = "Confirm_text";
             this.Confirm_text.PasswordChar = '*';
-            this.Confirm_text.Size = new System.Drawing.Size(250, 24);
+            this.Confirm_text.Size = new System.Drawing.Size(285, 29);
             this.Confirm_text.TabIndex = 27;
+            this.Confirm_text.TextChanged += new System.EventHandler(this.Confirm_text_TextChanged);
             // 
             // Email_text
             // 
             this.Email_text.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(78)))));
             this.Email_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Email_text.ForeColor = System.Drawing.Color.White;
-            this.Email_text.Location = new System.Drawing.Point(67, 464);
+            this.Email_text.Location = new System.Drawing.Point(77, 580);
+            this.Email_text.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Email_text.Name = "Email_text";
             this.Email_text.PasswordChar = '*';
-            this.Email_text.Size = new System.Drawing.Size(250, 24);
+            this.Email_text.Size = new System.Drawing.Size(285, 29);
             this.Email_text.TabIndex = 35;
             // 
             // label9
@@ -212,18 +223,18 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(63, 433);
+            this.label9.Location = new System.Drawing.Point(72, 541);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(72, 24);
+            this.label9.Size = new System.Drawing.Size(91, 29);
             this.label9.TabIndex = 34;
             this.label9.Text = "E-MAIL";
             // 
             // Signup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(78)))));
-            this.ClientSize = new System.Drawing.Size(924, 601);
+            this.ClientSize = new System.Drawing.Size(1056, 751);
             this.Controls.Add(this.Email_text);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -240,6 +251,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label6);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Signup";
             this.Text = "Signup";
             this.ResumeLayout(false);
--- Project_FileContentExplorer/Project_FileContentExplorer/Signup.cs
+++ Project_FileContentExplorer/Project_FileContentExplorer/Signup.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -20,6 +21,94 @@ namespace Project_FileContentExplorer
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void CheckID_Btn_Click(object sender, EventArgs e)
+        {
+            //아이디가 6자 미만일 경우
+            if (ID_text.Text.Length < 6)
+            {
+                MessageBox.Show("아이디는 6자 이상 입력하셔야합니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+            Http http = new Http("ID_Check.php");
+            http.PostParam.Append("ID=" + ID_text.Text);
+
+            http.Data = UTF8Encoding.UTF8.GetBytes(http.PostParam.ToString());
+
+            string result = http.Client_To_Server();
+
+            if (result.Equals("-1"))//이미 있는 아이디인 경우
+            {
+                ID_Error.Visible = true;
+
+            }
+            else if (result.Equals("1"))//아닌 경우
+            {
+                CheckID_Btn.BackColor = Color.FromArgb(12, 180, 82);
+                CheckID_Btn.Enabled = false;
+                ID_Error.Text = "Available ID";
+                ID_Error.ForeColor = Color.Green;
+                ID_Error.Visible = true;
+                ID_text.ReadOnly = true;
+            }
+        }
+
+        private void ID_text_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || Char.IsLetter(e.KeyChar)) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void ID_text_Leave(object sender, EventArgs e)
+        {
+            Regex regex = new Regex(@"[a-zA-Z0-9]");
+            Boolean ismatch = regex.IsMatch(ID_text.Text);
+
+            if (ID_text.Text.Length == 0)
+            {
+                MessageBox.Show("아이디를 입력해주세요.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ID_text.Focus();
+            }
+
+            if (!ismatch)
+            {
+                MessageBox.Show("아이디는 영문자,숫자만 가능합니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ID_text.Text = "";
+                ID_text.Focus();
+            }
+        }
+
+        private void SingUp_Btn_Click(object sender, EventArgs e)
+        {
+            if (PW_text.Text.Length < 8)
+            {
+                MessageBox.Show("비밀번호는 8자 이상 입력하셔야합니다.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                PW_text.Focus();
+            }
+
+        }
+
+        private void Confirm_text_TextChanged(object sender, EventArgs e)
+        {
+            if (PW_text.Text.Equals(Confirm_text.Text))
+            {
+
+                PW_Check_Label.Text = "Correct!";
+                PW_Check_Label.ForeColor = Color.Green;
+                PW_Check_Label.Visible = true;
+            }
+            else
+            {
+                PW_Check_Label.Text = "* Passwords don't match!";
+                PW_Check_Label.ForeColor = Color.Red;
+                PW_Check_Label.Visible = true;
+            }
         }
     }
 }
